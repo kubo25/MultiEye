8fba8bddadda63db9d4ecfcef7db22ac1353dbfc
kubo25 <jakubhucko@gmail.com>
2017-02-09 12:12:44 +0100

    Changed code displaying
    Monaco and Cytoscape for each code are now wrapped together. Code
    windows now scale to prevent window overflow.

--- js/codeWindow.js
+++ js/codeWindow.js
@@ -2,20 +2,35 @@ const cytoscape = require("cytoscape");
 
 class CodeWindow{  
     constructor(){
-        var tempEditor;
+        overflowDetection = true;
+        
+        let tempEditor;
+        
+        let codeWrapper = document.getElementById("codeWrapper");
+        
+        this.cyWrapper = document.createElement("div");
+        this.cyWrapper.className = "cyWrapper";
+        this.cyWrapper.id = "cyWrapper" + codeWindows.length;
+        
+        codeWrapper.appendChild(this.cyWrapper);
+        
+        let scaleWrapper = document.createElement("div");
+        scaleWrapper.className = "scaleWrapper";
+        
+        this.cyWrapper.appendChild(scaleWrapper);
         
         this.editor = null;
         this.editorId = "editor" + codeWindows.length;
+        
         this.cy = null;
         this.cyId = "cy" + codeWindows.length;
         
-        let bottom = document.getElementById("bottom");
         let editorDiv = document.createElement("div");
         
         editorDiv.id = this.editorId;
         editorDiv.className = "editor";
         
-        bottom.appendChild(editorDiv);
+        scaleWrapper.appendChild(editorDiv);
         
         amdRequire.config({ paths: { 'vs': 'node_modules/monaco-editor/min/vs' }});
 
@@ -29,12 +44,8 @@ class CodeWindow{
                 language: 'csharp'
             });
             
-            if(first){
-                let editor = document.getElementById("editor0");
-                let cy = document.getElementById("cy0");
-                
-                editor.parentNode.removeChild(editor);
-                cy.parentNode.removeChild(cy);
+            if(first){               
+                codeWrapper.removeChild(document.getElementById("cyWrapper0"));
                 
                 codeWindows = [];
                 first = false;
@@ -44,19 +55,13 @@ class CodeWindow{
         if(tempEditor != null){
             this.editor = tempEditor;
         }
-                
-        let height = document.getElementById(editorDiv.id).clientHeight;
-        let width = document.getElementById(editorDiv.id).clientWidth;
-        
-        let cyWrapper = document.getElementById("cyWrapper");
+
         let cyDiv = document.createElement("div");
         
         cyDiv.id = this.cyId;
         cyDiv.className = "cytoscape";
-        cyDiv.style.height = height + "px";
-        cyDiv.style.width = width + "px";
-        
-        cyWrapper.appendChild(cyDiv);
+
+        scaleWrapper.appendChild(cyDiv);
         
         this.cy = cytoscape({
 			container: document.getElementById(cyDiv.id),
@@ -90,16 +95,72 @@ class CodeWindow{
 		});
     }
     
-    setSize(newHeight, newWidth){
-        let editorWindow = document.getElementById(this.editorId);
-        let cy = document.getElementById(this.cyId);
+    addText(data){
+        let lines = this.cyWrapper.querySelector(".view-lines");
+        
+        let obj = this;
+    
+        let observer = new MutationObserver(function(mutations) {
+            mutations.forEach(function(mutation) {
+                let newHeight = mutation.target.scrollHeight;
+                let newWidth = mutation.target.scrollWidth + 20;
+                
+                obj.setSize(newHeight, newWidth);
+            });    
+        });
 
-        editorWindow.style.height = newHeight;
-        editorWindow.style.width = newWidth;
+        let config = { attributes: true, childList: false, characterData: false };
+        observer.observe(lines, config);    
+
+        this.editor.setValue(data);
+
+        setTimeout(function(){
+            observer.disconnect();
+        }, 1000);
+    }
+    
+    setSize(newHeight, newWidth){
+        let transform = "scale(" + scale + ")";
+        
+        this.cyWrapper.firstChild.style.transform = transform;
+        
+        let editorWindow = this.cyWrapper.firstChild.firstChild
+        let cy = this.cyWrapper.firstChild.lastChild;
+
+        editorWindow.style.height = newHeight + "px";
+        editorWindow.style.width = newWidth + "px";
 
         this.editor.layout();
 
-        cy.style.height = newHeight;
-        cy.style.width = newWidth;
+        cy.style.height = newHeight + "px";
+        cy.style.width = newWidth + "px";
+        
+        this.cyWrapper.style.height = (newHeight * scale) + "px";
+        this.cyWrapper.style.width = (newWidth * scale) + "px";
+                        
+        if(overflowDetection && document.body.clientHeight > window.innerHeight){
+            overflowDetection = false;
+            
+            let codeWrapper = document.getElementById("codeWrapper");
+            
+            while(document.body.clientHeight > window.innerHeight){
+                scale /= 2;
+                
+                transform = "scale(" + scale + ")";
+                        
+                for(const cy of codeWrapper.childNodes){
+                    cy.firstChild.style.transform = transform;
+
+                    console.log(cy.clientHeight + " " + cy.clientWidth);
+
+                    let tempHeight = (cy.clientHeight / 2);
+                    let tempWidth = (cy.clientWidth / 2);
+
+                    cy.style.height = tempHeight + "px";
+                    cy.style.width = tempWidth + "px";
+                }
+            }
+            
+        }
     }
 }
--- js/loadFile.js
+++ js/loadFile.js
@@ -25,37 +25,16 @@
 
         for (let i = 0; i < e.dataTransfer.files.length; i++) {
             let file = e.dataTransfer.files[i];
-            let data = fs.readFileSync(file.path, 'utf-8');  
+            let data = fs.readFileSync(file.path, 'utf-8'); 
             
-            codeWindows.push(new CodeWindow());
-            addText(codeWindowsCount, data);
+            let codeWindow = new CodeWindow();            
+            codeWindow.addText(data);
+            
+            codeWindows.push(codeWindow);
             
             codeWindowsCount++;
-            
         }
 
         return false;
     };
 })();
-
-function addText(id, data){  
-    let lines = document.querySelector("#" + codeWindows[id].editorId + " .view-lines");
-    
-    var observer = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
-            let newHeight = mutation.target.scrollHeight + "px";
-            let newWidth = (mutation.target.scrollWidth + 20) + "px";
-            
-            codeWindows[id].setSize(newHeight, newWidth);
-        });    
-    });
-    
-    var config = { attributes: true, childList: false, characterData: false };
-    observer.observe(lines, config);    
-    
-    codeWindows[id].editor.setValue(data);
-       
-    setTimeout(function(){
-        observer.disconnect();
-    }, 1000);
-}
